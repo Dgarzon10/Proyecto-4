3646d5be096c877f50251a0653106da6a5eb69a0
Dgarzon10 <dgarzonvilla@gmail.com>
2022-10-29 18:46:22 -0500

    Tablas de Tendencia
    Se añadieron dos tablas que contienen informes estadisticos relacionados a la tendencia tanto del entorno local como del global

--- App Servicio/javascript/graficos.js
+++ App Servicio/javascript/graficos.js
@@ -68,3 +68,24 @@ const myChart2 = new Chart(ctx2, {
         }
     }
 });
+const ctx3 = document.getElementById('myChart3');
+const myChart3 = new Chart(ctx3, {
+    type: 'doughnut',
+    data : {
+        labels: [
+          'Red',
+          'Blue',
+          'Yellow'
+        ],
+        datasets: [{
+          label: 'My First Dataset',
+          data: [300, 50, 100],
+          backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+            'rgb(255, 205, 86)'
+          ],
+          hoverOffset: 4
+        }]
+      },
+});
